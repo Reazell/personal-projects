471da04005ad4ea888f3dadb5fab2887782dd76e
Reazell <kapitankopytko@gmail.com>
2018-02-05 14:19:53 +0100

    c# test

---
+++ C#/C# test/Kolokwium/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium
+{
+    class Email : INotatka
+    {
+        private string emailAddress;
+
+        public void wydrukujPotwierdzenie()
+        {
+            Console.WriteLine("Wyslano potwierdzenie zamowienia na adres " + emailAddress);
+        }
+    }
+}
