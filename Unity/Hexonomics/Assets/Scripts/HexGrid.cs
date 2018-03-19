3db10dd38bdff1bde58ecdc13fd805f3254ae685
Reazell <kapitankopytko@gmail.com>
2018-03-19 15:27:32 +0100

    Connecting neighbour hexes to each other.

--- Unity/Hexonomics/Assets/Scripts/HexGrid.cs
+++ Unity/Hexonomics/Assets/Scripts/HexGrid.cs
@@ -56,7 +56,7 @@ public class HexGrid : MonoBehaviour
     }
 
 
-    private void CreateCell(int x, int z, int i)
+    void CreateCell(int x, int z, int i)
     {
         Vector3 position;
         position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
@@ -69,6 +69,31 @@ public class HexGrid : MonoBehaviour
         cell.transform.localPosition = position;
         cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
         cell.color = defaultColor;
+
+        if (x > 0)
+        {
+            cell.SetNeighbour(HexDirection.W, cells[i - 1]);
+        }
+
+        if (z > 0)
+        {
+            if ((z & 1) == 0)
+            {
+                cell.SetNeighbour(HexDirection.SE, cells[i - width]);
+                if (x > 0)
+                {
+                    cell.SetNeighbour(HexDirection.SW, cells[i - width - 1]);
+                }
+            }
+            else
+            {
+                cell.SetNeighbour(HexDirection.SW, cells[i - width]);
+                if (x < width - 1)
+                {
+                    cell.SetNeighbour(HexDirection.SE, cells[i-width + 1]);
+                }
+            }
+        }
 
         Text label = Instantiate<Text>(cellLabelPrefab);
         label.rectTransform.SetParent(gridCanvas.transform, false);
