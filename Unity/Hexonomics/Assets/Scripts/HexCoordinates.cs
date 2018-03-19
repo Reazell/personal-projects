2280c9ff69d97fb1a284db21ebc15549d9e1e1b2
Reazell <kapitankopytko@gmail.com>
2018-03-19 13:35:28 +0100

    Hex Coordinates system serialization

---
+++ Unity/Hexonomics/Assets/Scripts/HexCoordinates.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct HexCoordinates
+{
+    public int X { get; private set; }
+    public int Z { get; private set; }
+
+    public HexCoordinates(int x, int z) : this()
+    {
+        X = x;
+        Z = z;
+    }
+
+    public static HexCoordinates FromOffsetCoordinates(int x, int z)
+    {
+        return new HexCoordinates(x, z);
+    }
+
+    public override string ToString()
+    {
+        return "(" + X.ToString() + ", " + Z.ToString() + ")";
+    }
+
+    public string ToStringOnSeparateLines()
+    {
+        return X.ToString() + "\n" + Z.ToString();
+    }
+}
