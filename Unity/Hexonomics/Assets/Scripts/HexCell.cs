4e17caabaeef69faed10bb3efb3a6af0f3dc785a
Reazell <kapitankopytko@gmail.com>
2018-03-19 16:45:17 +0100

    Dynamic cell labels

--- Unity/Hexonomics/Assets/Scripts/HexCell.cs
+++ Unity/Hexonomics/Assets/Scripts/HexCell.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HexCell : MonoBehaviour
 {
     public HexCoordinates coordinates;
+    public RectTransform uiRect;
 
     public Color color;
 
@@ -17,6 +18,10 @@ public class HexCell : MonoBehaviour
             Vector3 position = transform.localPosition;
             position.y = value * HexMetrics.elevationStep;
             transform.localPosition = position;
+
+            Vector3 uiPosition = uiRect.localPosition;
+            uiPosition.z = elevation * -HexMetrics.elevationStep;
+            uiRect.localPosition = uiPosition;
         }
     }
 
