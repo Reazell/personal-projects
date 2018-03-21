2171b9bf4470cb736f9302d96b937e4f42492be4
Reazell <kapitankopytko@gmail.com>
2018-03-21 18:42:47 +0100

    Perturb cliff/slope fill

--- Unity/Hexonomics/Assets/Scripts/HexMesh.cs
+++ Unity/Hexonomics/Assets/Scripts/HexMesh.cs
@@ -221,7 +221,7 @@ public class HexMesh : MonoBehaviour {
 		if (b < 0) {
 			b = -b;
 		}
-		Vector3 boundary = Vector3.Lerp(begin, right, b);
+		Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(right), b);
 		Color boundaryColor = Color.Lerp(beginCell.color, rightCell.color, b);
 
 		TriangulateBoundaryTriangle(
@@ -248,7 +248,7 @@ public class HexMesh : MonoBehaviour {
 		if (b < 0) {
 			b = -b;
 		}
-		Vector3 boundary = Vector3.Lerp(begin, left, b);
+		Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(left), b);
 		Color boundaryColor = Color.Lerp(beginCell.color, leftCell.color, b);
 
 		TriangulateBoundaryTriangle(
@@ -271,10 +271,10 @@ public class HexMesh : MonoBehaviour {
 		Vector3 left, HexCell leftCell,
 		Vector3 boundary, Color boundaryColor
 	) {
-		Vector3 v2 = HexMetrics.TerraceLerp(begin, left, 1);
+		Vector3 v2 = Perturb(HexMetrics.TerraceLerp(begin, left, 1));
 		Color c2 = HexMetrics.TerraceLerp(beginCell.color, leftCell.color, 1);
 
-		AddTriangle(begin, v2, boundary);
+		AddTriangleUnperturbed(Perturb(begin), v2, boundary);
 		AddTriangleColor(beginCell.color, c2, boundaryColor);
 
 		for (int i = 2; i < HexMetrics.terraceSteps; i++) {
@@ -282,11 +282,11 @@ public class HexMesh : MonoBehaviour {
 			Color c1 = c2;
 			v2 = HexMetrics.TerraceLerp(begin, left, i);
 			c2 = HexMetrics.TerraceLerp(beginCell.color, leftCell.color, i);
-			AddTriangle(v1, v2, boundary);
+			AddTriangleUnperturbed(Perturb(v1), v2, boundary);
 			AddTriangleColor(c1, c2, boundaryColor);
 		}
 
-		AddTriangle(v2, left, boundary);
+		AddTriangleUnperturbed(v2, Perturb(left), boundary);
 		AddTriangleColor(c2, leftCell.color, boundaryColor);
 	}
 
@@ -367,4 +367,15 @@ public class HexMesh : MonoBehaviour {
 		position.z += (sample.z * 2f - 1f) * HexMetrics.cellPerturbStrength;
 		return position;
 	}
+
+    void AddTriangleUnperturbed(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex +1);
+        triangles.Add(vertexIndex + 2);
+    }
 }
