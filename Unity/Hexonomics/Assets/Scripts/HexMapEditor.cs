e10917a12536c1854f7a77279c2515e268c9f177
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2018-03-22 16:05:43 +0100

    Paint brush size editor

--- Unity/Hexonomics/Assets/Scripts/HexMapEditor.cs
+++ Unity/Hexonomics/Assets/Scripts/HexMapEditor.cs
@@ -6,6 +6,7 @@ public class HexMapEditor : MonoBehaviour {
 	public Color[] colors;
 	public HexGrid hexGrid;
 	int activeElevation;
+    private int brushSize;
 	Color activeColor;
 
     private bool applyColor;
@@ -39,21 +40,44 @@ public class HexMapEditor : MonoBehaviour {
 		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast(inputRay, out hit)) {
-			EditCell(hexGrid.GetCell(hit.point));
+			EditCells(hexGrid.GetCell(hit.point));
 		}
 	}
 
-	void EditCell (HexCell cell) {
-	    if (applyColor) {
-	        cell.Color = activeColor;
-	    }
+    private void EditCells(HexCell center) {
+        int centerX = center.coordinates.X;
+        int centerZ = center.coordinates.Z;
 
-	    if (applyElevation) {
-	        cell.Elevation = activeElevation;
-	    }
-	}
+        for (int r = 0, z = centerZ - brushSize; z <= centerZ; z++, r++) {
+            for (int x = centerX - r; x <= centerX + brushSize; x++) {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x,z)));
+            }
+        }
+
+        for (int r = 0, z = centerZ + brushSize; z > centerZ; z--, r++) {
+            for (int x = centerX - brushSize; x <= centerX + r; x++) {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x,z)));
+            }
+        }
+    }
+
+    void EditCell (HexCell cell) {
+        if (cell) {
+            if (applyColor) {
+                cell.Color = activeColor;
+            }
+
+            if (applyElevation) {
+                cell.Elevation = activeElevation;
+            }
+        }
+    }
 
     public void SetApplyElevation(bool toggle) {
         applyElevation = toggle;
+    }
+
+    public void SetBrushSize(float size) {
+        brushSize = (int) size;
     }
 }
