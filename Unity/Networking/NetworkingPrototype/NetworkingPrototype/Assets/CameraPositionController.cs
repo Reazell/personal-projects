d3d616c2c8cb8edd171c490315ff3ade1468ddfc
Szymon Przybyszewski <szymon.przybyszewski94@gmail.com>
2018-10-09 14:52:03 +0200

    [WIP] Networking Prototype

---
+++ Unity/Networking/NetworkingPrototype/NetworkingPrototype/Assets/CameraPositionController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraPositionController : NetworkBehaviour {
+
+	public bool manualCameraPositionControl;
+	[SerializeField] GameObject player;
+	[SerializeField] private NetworkIdentity networkIdentity;
+	Transform cameraContainer;
+	float xPosition;
+	float yPosition;
+	float zPosition;
+
+	void Start() {
+		cameraContainer = GetComponent<Transform>();
+	}
+	public void AdjustPositionX(float newXPosition) {
+		xPosition = newXPosition;
+	}
+	public void AdjustPositionY(float newYPosition) {
+		yPosition = newYPosition;
+	}
+	public void AdjustPositionZ(float newZPosition) {
+		zPosition = newZPosition;
+	}
+
+	void Update() {
+		if (player.tag == "Player") {
+			if (manualCameraPositionControl) {
+				var newPosition = new Vector3(xPosition, yPosition, zPosition);
+				player.transform.position = newPosition;
+			}
+		}
+	}
+}
